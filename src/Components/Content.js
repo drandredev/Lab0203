8b6da92b12a75964136346cff7e9da14e5abd484
drandredev <drandredev@drandredev.localdomain>
2021-03-23 20:36:37 -0500

    Punto 3

---
+++ src/Components/Content.js
@@ -0,0 +1,12 @@
+const Content = (props) => {
+
+    return(
+    <div>
+      <Part .../>
+      <Part .../>
+      <Part .../>
+    </div>
+    )
+}
+
+export default Content
---
+++ src/Components/Total.js
@@ -0,0 +1,8 @@
+const Total = (props) => {
+
+    return(
+
+    )
+}
+
+export default Total
